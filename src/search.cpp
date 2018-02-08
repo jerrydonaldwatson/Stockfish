bb79c6d6cd24b2ea64a0ebb6b76dc9deb2224c1d
Jerry Donald Watson <jdw69@eed21049.eng.cam.ac.uk>
2018-02-08 12:13:43 +0000

    Vary futility margin according to ply
    More aggressive pruning is doing far better at STC than at LTC, so for longer searchs use a higher futility margin near the leaves.
    
    Intuitively this makes sense to me, as futility is a depth/accuracy trade-off. At lower depths it's more important to get to the next ply quickly, whereas at higher depths it may be more important to be accurate.
    
    Also has the consequence of a little more pruning in non-PV nodes as the seldepth is lower.
    
    Bench: 5183374

--- src/search.cpp
+++ src/search.cpp
@@ -68,7 +68,7 @@ namespace {
 
   // Razoring and futility margin based on depth
   const int razor_margin = 600;
-  Value futility_margin(Depth d) { return Value(150 * d / ONE_PLY); }
+  Value futility_margin(Depth d, int ply) { return Value((120 + 2*ply) * d / ONE_PLY); }
 
   // Futility and reductions lookup tables, initialized at startup
   int FutilityMoveCounts[2][16]; // [improving][depth]
@@ -683,7 +683,7 @@ namespace {
     // Step 7. Futility pruning: child node (skipped when in check)
     if (   !rootNode
         &&  depth < 7 * ONE_PLY
-        &&  eval - futility_margin(depth) >= beta
+        &&  eval - futility_margin(depth, ss->ply) >= beta
         &&  eval < VALUE_KNOWN_WIN)  // Do not return unproven wins
         return eval;
 

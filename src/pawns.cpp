580a6f45f4542afa6b63b3b30659a15d6572ffcb
Jerry Donald Watson <j1.donald1@gmail.com>
2018-02-10 09:04:51 +0000

    Try a more sophisticated implementation consdering B1 also
    Bench: 5583584

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -284,10 +284,13 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
 
   // If we can castle use the bonus after the castling if it is bigger
   if (pos.can_castle(MakeCastling<Us, KING_SIDE>::right))
-      bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_G1)) - Value(10));
+      bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_G1)) - Value(8));
 
   if (pos.can_castle(MakeCastling<Us, QUEEN_SIDE>::right))
-      bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_C1)) - Value(10));
+  {
+      Value castleBonus = std::max(shelter_storm<Us>(pos, relative_square(Us, SQ_C1)) - Value(8), shelter_storm<Us>(pos, relative_square(Us, SQ_B1)) - Value(16));
+      bonus = std::max(bonus, castleBonus);
+  }
 
   return make_score(bonus, -16 * minKingPawnDistance);
 }

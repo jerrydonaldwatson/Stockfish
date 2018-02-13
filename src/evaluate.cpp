82f703793df3cbe34e7baab008d3b85a053621e3
Jerry Donald Watson <j1.donald1@gmail.com>
2018-02-13 23:43:53 +0000

    Even more lever bonus, compensate in the weak term as some levers could be counted there as well
    Bench: 5068097

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -487,9 +487,9 @@ namespace {
 
         kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                      + 102 * kingAdjacentZoneAttacksCount[Them]
-                     + 191 * popcount(kingRing[Us] & weak)
+                     + 185 * popcount(kingRing[Us] & weak)
                      + 143 * popcount(pos.pinned_pieces(Us) | unsafeChecks)
-                     +  64 * levers
+                     +  72 * levers
                      - 848 * !pos.count<QUEEN>(Them)
                      -   9 * mg_value(score) / 8
                      +  40;

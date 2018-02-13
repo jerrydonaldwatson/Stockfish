3e1c64f2839376ba4ea372372ebbea6b855cec1b
Jerry Donald Watson <j1.donald1@gmail.com>
2018-02-13 21:00:17 +0000

    Let minKingPawnDistance consider enemy pawns, and compensate
    Bench: 5429885

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -276,8 +276,8 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
   castlingRights[Us] = pos.can_castle(Us);
   int minKingPawnDistance = 0;
 
-  Bitboard pawns = pos.pieces(Us, PAWN);
-  while (!(DistanceRingBB[ksq][minKingPawnDistance++] & pawns) && minKingPawnDistance < 5) {}
+  Bitboard pawns = pos.pieces(PAWN);
+  while (!(DistanceRingBB[ksq][minKingPawnDistance++] & pawns) && minKingPawnDistance < 6) {}
 
   Value bonus = shelter_storm<Us>(pos, ksq);
 
@@ -288,7 +288,7 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
   if (pos.can_castle(MakeCastling<Us, QUEEN_SIDE>::right))
       bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_C1)));
 
-  return make_score(bonus, -16 * minKingPawnDistance);
+  return make_score(bonus, -20 * minKingPawnDistance);
 }
 
 // Explicit template instantiation

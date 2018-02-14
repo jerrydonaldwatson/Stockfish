8d044f5c25f27ab0b891849f88cf3df48625c0ba
Jerry Donald Watson <j1.donald1@gmail.com>
2018-02-14 01:46:19 +0000

    Try the opposite
    Bench: 5537818

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -487,9 +487,9 @@ namespace {
 
         kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                      + 102 * kingAdjacentZoneAttacksCount[Them]
-                     + 185 * popcount(kingRing[Us] & weak)
+                     + 197 * popcount(kingRing[Us] & weak)
                      + 143 * popcount(pos.pinned_pieces(Us) | unsafeChecks)
-                     +  72 * levers
+                     +  56 * levers
                      - 848 * !pos.count<QUEEN>(Them)
                      -   9 * mg_value(score) / 8
                      +  40;

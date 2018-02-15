78a3a5d9c958e9ddd01696c6eb57cdbf58c28af9
Jerry Donald Watson <j1.donald1@gmail.com>
2018-02-15 07:52:11 +0000

    Modify kingDanger by # of rooks
    Bench: 5232171

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -488,7 +488,8 @@ namespace {
                      + 143 * popcount(pos.pinned_pieces(Us) | unsafeChecks)
                      - 848 * !pos.count<QUEEN>(Them)
                      -   9 * mg_value(score) / 8
-                     +  40;
+                     +  20 * pos.count<ROOK>(Them)
+                     +  20;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
         if (kingDanger > 0)

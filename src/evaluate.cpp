0314392c6dc01c1a1c7cc876bfb69fa0eca502dc
Jerry Donald Watson <jdw69@eed21049.eng.cam.ac.uk>
2018-02-19 17:27:42 +0000

    Correct compensation from bench
    Bench: 5411005

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -770,7 +770,8 @@ namespace {
                       - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (pe->pawn_asymmetry() + kingDistance) + 12 * pos.count<PAWN>() + 4 * pe->pawn_spread() - 140;
+    int initiative = 8 * (pe->pawn_asymmetry() + kingDistance) + 12 * pos.count<PAWN>() + 4 * pe->pawn_spread() - 144;
+    dbg_mean_of(pe->pawn_spread());
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

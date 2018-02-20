392360b708c20c54267e1c9b4cf06a6bbdd18cc2
Jerry Donald Watson <jdw69@eed21049.eng.cam.ac.uk>
2018-02-20 12:18:28 +0000

    Don't penalize unsupported doubled pawns
    Bench: 5126290

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -41,7 +41,7 @@ namespace {
   Score Connected[2][2][3][RANK_NB];
 
   // Doubled pawn penalty
-  const Score Doubled = S(9, 18);
+  const Score Doubled = S(18, 38);
 
   // Weakness of our pawn shelter in front of the king by [isKingFile][distance from edge][rank].
   // RANK_1 = 0 is used for files where we have no pawns or our pawn is behind our king.
@@ -176,12 +176,8 @@ namespace {
         else if (backward)
             score -= Backward, e->weakUnopposed[Us] += !opposed;
 
-        if (doubled)
-        {
-			score -= Doubled;
-		    if (!(supported | phalanx))
-                score -= Doubled;
-		} 
+        if (doubled && !(supported | phalanx))
+            score -= Doubled;
     }
 
     return score;

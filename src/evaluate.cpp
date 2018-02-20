9b73a8364a2a0c4b040109d4e4fbc10b9f44a6f9
Jerry Donald Watson <jdw69@eed21049.eng.cam.ac.uk>
2018-02-20 18:48:04 +0000

    Extra bonus for our rook behind passed pawn
    Bench: 4915603

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -689,7 +689,7 @@ namespace {
 
                 // If there aren't any enemy attacks, assign a big bonus. Otherwise
                 // assign a smaller bonus if the block square isn't attacked.
-                int k = !unsafeSquares ? 20 : !(unsafeSquares & blockSq) ? 9 : 0;
+                int k = !unsafeSquares ? 20 : !(unsafeSquares & blockSq) ? 9 : 2 * bool(pos.pieces(Us, ROOK) & bb);
 
                 // If the path to the queen is fully defended, assign a big bonus.
                 // Otherwise assign a smaller bonus if the block square is defended.

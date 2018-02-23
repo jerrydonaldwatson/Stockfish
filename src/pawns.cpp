8b19d7d362bbe20772e472bdc52ac98cb53c1b99
Jerry Donald Watson <jdw69@eed21049.eng.cam.ac.uk>
2018-02-23 13:13:23 +0000

    King shelter adjusted for king rook
    Bench: 5425445

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -46,14 +46,14 @@ namespace {
   // Weakness of our pawn shelter in front of the king by [isKingFile][distance from edge][rank].
   // RANK_1 = 0 is used for files where we have no pawns or our pawn is behind our king.
   const Value ShelterWeakness[][int(FILE_NB) / 2][RANK_NB] = {
-    { { V( 98), V(20), V(11), V(42), V( 83), V( 84), V(101) }, // Not On King file
-      { V(103), V( 8), V(33), V(86), V( 87), V(105), V(113) },
-      { V(100), V( 2), V(65), V(95), V( 59), V( 89), V(115) },
-      { V( 72), V( 6), V(52), V(74), V( 83), V( 84), V(112) } },
-    { { V(105), V(19), V( 3), V(27), V( 85), V( 93), V( 84) }, // On King file
-      { V(121), V( 7), V(33), V(95), V(112), V( 86), V( 72) },
-      { V(121), V(26), V(65), V(90), V( 65), V( 76), V(117) },
-      { V( 79), V( 0), V(45), V(65), V( 94), V( 92), V(105) } }
+    { { V(103), V(20), V(11), V(42), V( 83), V( 84), V(101) }, // Not On King file
+      { V(108), V( 8), V(33), V(86), V( 87), V(105), V(113) },
+      { V(105), V( 2), V(65), V(95), V( 59), V( 89), V(115) },
+      { V( 77), V( 6), V(52), V(74), V( 83), V( 84), V(112) } },
+    { { V(110), V(19), V( 3), V(27), V( 85), V( 93), V( 84) }, // On King file
+      { V(126), V( 7), V(33), V(95), V(112), V( 86), V( 72) },
+      { V(126), V(26), V(65), V(90), V( 65), V( 76), V(117) },
+      { V( 84), V( 0), V(45), V(65), V( 94), V( 92), V(105) } }
   };
 
   // Danger of enemy pawns moving toward our king by [type][distance from edge][rank].
@@ -77,10 +77,13 @@ namespace {
       { V(23),  V(  29), V(  96), V(41), V(15) },
       { V(21),  V(  23), V( 116), V(41), V(15) } }
   };
+  
+  // Penalty for king a long way from its own pawns
+  const Value DistantPawn = V(4);
 
   // Max bonus for king safety. Corresponds to start position with all the pawns
   // in front of the king and no enemy pawn on the horizon.
-  const Value MaxSafetyBonus = V(258);
+  const Value MaxSafetyBonus = V(273);
 
   #undef S
   #undef V
@@ -249,6 +252,9 @@ Value Entry::shelter_storm(const Position& pos, Square ksq) {
   {
       b = ourPawns & file_bb(f);
       Rank rkUs = b ? relative_rank(Us, backmost_sq(Us, b)) : RANK_1;
+     
+      if (b)
+          safety -= DistantPawn * std::min(distance(ksq,backmost_sq(Us, b)), 3);
 
       b = theirPawns & file_bb(f);
       Rank rkThem = b ? relative_rank(Us, frontmost_sq(Them, b)) : RANK_1;

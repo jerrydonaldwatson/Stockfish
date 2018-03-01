4bace032352638dfd9de33d506f9cacef09f9887
Jerry Donald Watson <jdw69@eed21049.eng.cam.ac.uk>
2018-03-01 15:33:54 +0000

    Less penalty
    Bench: 5877928

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -472,8 +472,8 @@ namespace {
                      + 143 * popcount(pinned | unsafeChecks)
                      - 848 * !pos.count<QUEEN>(Them)
                      -   9 * mg_value(score) / 8
-                     +  64 * !pos.can_castle(Us)
-                     -  12;
+                     +  32 * !pos.can_castle(Us)
+                     +  16;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
         if (kingDanger > 0)

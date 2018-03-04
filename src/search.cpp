47e46975652550eb2ea2039f75494e3e423732bc
Jerry Donald Watson <j1.donald1@gmail.com>
2018-03-04 22:51:22 +0000

    Use static eval to modify MCP
    Bench: 5564634

--- src/search.cpp
+++ src/search.cpp
@@ -856,7 +856,7 @@ moves_loop: // When in check, search starts from here
       givesCheck = gives_check(pos, move);
 
       moveCountPruning =   depth < 16 * ONE_PLY
-                        && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY];
+                        && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY] + std::max((int)(ss->staticEval - alpha) / 200, - depth / ONE_PLY);
 
       // Step 13. Extensions
 

91992b78e5f047abe12261adefc1f5e3f095ebb1
Jerry Donald Watson <jdw69@eed21049.eng.cam.ac.uk>
2018-03-05 18:13:00 +0000

    Try 24
    Bench: 5851320

--- src/search.cpp
+++ src/search.cpp
@@ -878,7 +878,7 @@ moves_loop: // When in check, search starts from here
               extension = ONE_PLY;
       }
       else if (    givesCheck // Check extension
-               &&  (depth >= 12 * ONE_PLY || moveCount < 12)
+               &&  (depth >= 12 * ONE_PLY || moveCount < 24)
                &&  pos.see_ge(move))
           extension = ONE_PLY;
 

f8e8c1f094730820c7faa1ce11546766b2ce8f7d
Jerry Donald Watson <j1.donald1@gmail.com>
2018-03-09 08:27:59 +0000

    Take 3. Use ttValue for prelim search
    Bench: 5334476

--- src/search.cpp
+++ src/search.cpp
@@ -877,9 +877,9 @@ moves_loop: // When in check, search starts from here
       {
           Value rBeta = std::max(ttValue - 2 * depth / ONE_PLY, -VALUE_MATE);
           ss->excludedMove = move;
-          value = search<NonPV>(pos, ss, beta - 1, beta, ONE_PLY, cutNode, true);
+          value = search<NonPV>(pos, ss, ttValue - 1, ttValue, ONE_PLY, cutNode, true);
           
-          if (value < beta)
+          if (value < ttValue)
               value = search<NonPV>(pos, ss, rBeta - 1, rBeta, depth / 2, cutNode, true);
           ss->excludedMove = MOVE_NONE;
 

ffcd050398b2929360180c457ecc741d2b171739
Jerry Donald Watson <j1.donald1@gmail.com>
2018-03-10 11:14:16 +0000

    Adjust probcut rBeta with improving
    Bench: 5598275

--- src/search.cpp
+++ src/search.cpp
@@ -761,7 +761,7 @@ namespace {
     {
         assert(is_ok((ss-1)->currentMove));
 
-        Value rbeta = std::min(beta + 200, VALUE_INFINITE);
+        Value rbeta = std::min(beta + 225 - 50 * improving, VALUE_INFINITE);
         MovePicker mp(pos, ttMove, rbeta - ss->staticEval, &thisThread->captureHistory);
         int probCutCount = 0;
 

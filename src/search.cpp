a1df1707e0d8e9f9c4c9ce5ac6adad1dac251b52
Jerry Donald Watson <j1.donald1@gmail.com>
2018-03-14 12:06:42 +0000

    Futility pruning only at depth < 6
    Bench: 5794455

--- src/search.cpp
+++ src/search.cpp
@@ -704,7 +704,7 @@ namespace {
 
     // Step 8. Futility pruning: child node (skipped when in check)
     if (   !rootNode
-        &&  depth < 7 * ONE_PLY
+        &&  depth < 6 * ONE_PLY
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;

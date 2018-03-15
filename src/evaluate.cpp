2de2395f9a5e69d9ed7c869279a55473d13031ea
Jerry Donald Watson <j1.donald1@gmail.com>
2018-03-15 18:56:40 +0000

    Include levers in mobility
    Bench: 5495190

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -260,7 +260,7 @@ namespace {
 
     // Squares occupied by those pawns, by our king, or controlled by enemy pawns
     // are excluded from the mobility area.
-    mobilityArea[Us] = ~(b | pos.square<KING>(Us) | pe->pawn_attacks(Them));
+    mobilityArea[Us] = ~(b | pos.square<KING>(Us) | (~pos.pieces(Us, PAWN) & pe->pawn_attacks(Them)));
 
     // Initialise attackedBy bitboards for kings and pawns
     attackedBy[Us][KING] = pos.attacks_from<KING>(pos.square<KING>(Us));

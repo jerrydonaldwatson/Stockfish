f9895f419285e47058727f7f3c328e28f183b129
Jerry Donald Watson <jdw69@eed21049.eng.cam.ac.uk>
2018-03-16 14:13:24 +0000

    Values derived from tuning
    Bench: 5198164

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -761,11 +761,11 @@ namespace {
                             && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int complexity =   8 * outflanking
-                    +  8 * pe->pawn_asymmetry()
+    int complexity =   9 * outflanking
+                    +  9 * pe->pawn_asymmetry()
                     + 12 * pos.count<PAWN>()
                     + 16 * pawnsOnBothFlanks
-                    + 48 * !pos.non_pawn_material()
+                    + 56 * !pos.non_pawn_material()
                     -136 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting

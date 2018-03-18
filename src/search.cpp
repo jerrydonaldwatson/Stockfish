d414c007e50bc0c1c41fce07165ecdfe67e7eb0a
Jerry Donald Watson <j1.donald1@gmail.com>
2018-03-18 00:16:42 +0000

    Don't allow immediate check extension in singular excursion search
    Bench; 5225855

--- src/search.cpp
+++ src/search.cpp
@@ -883,6 +883,7 @@ moves_loop: // When in check, search starts from here
       }
       else if (    givesCheck // Check extension
                && !moveCountPruning
+               && !excludedMove
                &&  pos.see_ge(move))
           extension = ONE_PLY;
 

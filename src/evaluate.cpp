78f5073deeed8ebb76bf9a29683fd3fb16f5913a
Jerry Donald Watson <j1.donald1@gmail.com>
2018-03-20 23:42:29 +0000

    Put all levers in Connectivty
    Bench: 5672477

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -602,7 +602,8 @@ namespace {
     }
 
     // Connectivity: ensure that knights, bishops, rooks, and queens are protected
-    b = (pos.pieces(Us) ^ pos.pieces(Us, PAWN, KING)) & attackedBy[Us][ALL_PIECES];
+    Bitboard levers = (pos.pieces(Us, PAWN) & attackedBy[Them][PAWN]) | (pos.pieces(Them, PAWN) & attackedBy[Us][PAWN]);
+    b = ((pos.pieces(Us) ^ pos.pieces(Us, PAWN, KING)) | levers) & attackedBy[Us][ALL_PIECES];
     score += Connectivity * popcount(b);
 
     if (T)

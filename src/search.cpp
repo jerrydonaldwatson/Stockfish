35f247564b4ccc4e7209e5de6d7a88dae80f448d
Jerry Donald Watson <j1.donald1@gmail.com>
2018-03-22 09:33:32 +0000

    Reduce statScore for branches with many extensions
    Bench: 6043704

--- src/search.cpp
+++ src/search.cpp
@@ -981,7 +981,8 @@ moves_loop: // When in check, search starts from here
                              + (*contHist[0])[movedPiece][to_sq(move)]
                              + (*contHist[1])[movedPiece][to_sq(move)]
                              + (*contHist[3])[movedPiece][to_sq(move)]
-                             - 4000;
+                             - 256 * (depth + ss->ply - thisThread->rootDepth) / ONE_PLY
+                             - 3665;
 
               // Decrease/increase reduction by comparing opponent's stat score
               if (ss->statScore >= 0 && (ss-1)->statScore < 0)

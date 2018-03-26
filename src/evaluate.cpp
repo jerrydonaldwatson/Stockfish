4cb5606f8968a6f5836998919aa94cb6d619d6ea
Jerry Donald Watson <j1.donald1@gmail.com>
2018-03-26 21:36:06 +0100

    No constant, aka zero
    Bench: 5851257

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -474,8 +474,7 @@ namespace {
                      + 191 * popcount(kingRing[Us] & weak)
                      + 143 * popcount(pinned | unsafeChecks)
                      - 848 * !pos.count<QUEEN>(Them)
-                     -   9 * mg_value(score) / 8
-                     +  40;
+                     -   9 * mg_value(score) / 8;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
         if (kingDanger > 0)

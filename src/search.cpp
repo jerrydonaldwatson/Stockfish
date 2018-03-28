094519ece5d68706245972ba1b6d305079104cec
Jerry Donald Watson <j1.donald1@gmail.com>
2018-03-28 10:24:04 +0100

    Less reduction for improving PV nodes
    Bench: 5632185

--- src/search.cpp
+++ src/search.cpp
@@ -960,7 +960,7 @@ moves_loop: // When in check, search starts from here
                   r -= ONE_PLY;
 
               // Decrease reduction for exact PV nodes
-              if (pvExact)
+              if (pvExact || (PvNode && improving))
                   r -= ONE_PLY;
 
               // Increase reduction if ttMove is a capture

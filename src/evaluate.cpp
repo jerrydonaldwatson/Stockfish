0e661e46527ef9e269dd2b7fe21d03a57016395e
Jerry Donald Watson <jdw69@eed21049.eng.cam.ac.uk>
2018-03-28 17:41:54 +0100

    Weight based on all pieces, not just our own
    Bench: 5330580

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -743,9 +743,9 @@ namespace {
     behind |= (Us == WHITE ? behind >> 16 : behind << 16);
 
     int bonus = popcount(safe) + popcount(behind & safe);
-    int weight = pos.count<ALL_PIECES>(Us) - 2 * pe->open_files();
+    int weight = pos.count<ALL_PIECES>() - 4 * pe->open_files();
 
-    Score score = make_score(bonus * weight * weight / 16, 0);
+    Score score = make_score(bonus * weight * weight / 64, 0);
 
     if (T)
         Trace::add(SPACE, Us, score);

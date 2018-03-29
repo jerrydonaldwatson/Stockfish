436b477d93f1bff8b4d563bbb0d0309d6fb0eb41
Jerry Donald Watson <j1.donald1@gmail.com>
2018-03-29 10:17:50 +0100

    Scale by 3/8
    Bench: 5356330

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -698,7 +698,7 @@ namespace {
         // Scale down bonus for candidate passers which need more than one
         // pawn push to become passed or have a pawn in front of them.
         if (!pos.pawn_passed(Us, s + Up))
-            bonus = (bonus * 5) / 8;
+            bonus = (bonus * 3) / 8;
         else if (pos.pieces(PAWN) & forward_file_bb(Us, s))
             bonus = bonus / 2;
 

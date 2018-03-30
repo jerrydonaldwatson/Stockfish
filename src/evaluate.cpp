81710ea4662641bc1b2f1aae924d485614f36104
Jerry Donald Watson <j1.donald1@gmail.com>
2018-03-30 15:27:03 +0100

    Try (4, 0)
    Bench: 5286042

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -175,7 +175,7 @@ namespace {
   constexpr Score PawnlessFlank      = S( 20, 80);
   constexpr Score RookOnPawn         = S(  8, 24);
   constexpr Score SliderOnQueen      = S( 42, 21);
-  constexpr Score ThreatByQueen      = S(  4,  4);
+  constexpr Score ThreatByQueen      = S(  4,  0);
   constexpr Score ThreatByPawnPush   = S( 47, 26);
   constexpr Score ThreatByRank       = S( 16,  3);
   constexpr Score ThreatBySafePawn   = S(175,168);

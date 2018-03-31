47a849cd756596ee11feebbf697a8f9c6666b9c2
Jerry Donald Watson <j1.donald1@gmail.com>
2018-03-31 01:07:50 +0100

    No stat score adjustment
    Bench: 6560064

--- src/search.cpp
+++ src/search.cpp
@@ -984,8 +984,15 @@ moves_loop: // When in check, search starts from here
                              + (*contHist[3])[movedPiece][to_sq(move)]
                              - 4000;
 
+              // Decrease/increase reduction by comparing opponent's stat score
+              if (ss->statScore >= 0 && (ss-1)->statScore < 0)
+                  r -= ONE_PLY;
+
+              else if ((ss-1)->statScore >= 0 && ss->statScore < 0)
+                  r += ONE_PLY;
+
               // Decrease/increase reduction for moves with a good/bad history
-              r = std::max(DEPTH_ZERO, (r / ONE_PLY - ss->statScore / 20000) * ONE_PLY);
+              r = std::max(DEPTH_ZERO, r);
           }
 
           Depth d = std::max(newDepth - r, ONE_PLY);

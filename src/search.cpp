a11799c012a297f9695b6da913d9fa61db9e1500
Jerry Donald Watson <jdw69@eed21049.eng.cam.ac.uk>
2018-04-03 10:32:40 +0100

    Try to prune some advanced pawn pushes in QS
    Bench: 5387924

--- src/search.cpp
+++ src/search.cpp
@@ -1270,9 +1270,8 @@ moves_loop: // When in check, search starts from here
       if (   !inCheck
           && !givesCheck
           &&  futilityBase > -VALUE_KNOWN_WIN
-          && !pos.advanced_pawn_push(move))
+          && (!pos.advanced_pawn_push(move) || pos.non_pawn_material(~pos.side_to_move()) >= Value(2500)))
       {
-          assert(type_of(move) != ENPASSANT); // Due to !pos.advanced_pawn_push
 
           futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
 

5f724ba3ccdd33865a14aa2b9afc281c6b10ca5a
Jerry Donald Watson <jdw69@eed21049.eng.cam.ac.uk>
2018-04-03 19:28:32 +0100

    Don't reduce extended checks
    Bench: 6185054

--- src/search.cpp
+++ src/search.cpp
@@ -955,6 +955,7 @@ moves_loop: // When in check, search starts from here
       // re-searched at full depth.
       if (    depth >= 3 * ONE_PLY
           &&  moveCount > 1
+          && !extension
           && (!captureOrPromotion || moveCountPruning))
       {
           Depth r = reduction<PvNode>(improving, depth, moveCount);

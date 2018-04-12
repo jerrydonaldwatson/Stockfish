326b087f18494f42abcd8cdc3f0fa686da2f978b
Jerry Donald Watson <j1.donald1@gmail.com>
2018-04-12 23:54:59 +0100

    Correct version
    Bench: 4902126

--- src/search.cpp
+++ src/search.cpp
@@ -906,7 +906,7 @@ moves_loop: // When in check, search starts from here
               }
 
               // Reduced depth of the next LMR search
-              int lmrDepth = std::max(newDepth - reduction<PvNode>(improving || !ttCapture, depth, moveCount), DEPTH_ZERO) / ONE_PLY;
+              int lmrDepth = std::max(newDepth - reduction<PvNode>(improving && !ttCapture, depth, moveCount), DEPTH_ZERO) / ONE_PLY;
 
               // Countermoves based pruning (~20 Elo)
               if (   lmrDepth < 3

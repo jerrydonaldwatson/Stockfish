8d6f7f15628b50668c95195bd6083df7d2719393
Jerry Donald Watson <jdw69@eed21049.eng.cam.ac.uk>
2018-04-17 13:28:26 +0100

    Less LMR for captures at low depths / move count
    Bench: 5227562

--- src/search.cpp
+++ src/search.cpp
@@ -960,7 +960,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction<PvNode>(improving, depth, moveCount);
 
           if (captureOrPromotion)
-              r -= r ? ONE_PLY : DEPTH_ZERO;
+              r -= r > ONE_PLY ? ONE_PLY : DEPTH_ZERO;
           else
           {
               // Decrease reduction if opponent's move count is high
